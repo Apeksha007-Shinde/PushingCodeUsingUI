26cb8712058f888887363a9ec93107c2d328fe29
Apeksha007-Shinde <apekshashinde1110@gmail.com>
2025-05-19 19:48:23 +0530

    TestUI-1212 || Pushing code to remote

---
+++ src/myDemo/TestUI.java
@@ -0,0 +1,9 @@
+package myDemo;
+
+public class TestUI {
+
+	public static void main(String[] args) {
+		System.out.println("Hi, we are pushing the code from UI(From eclipse instead of git) to github");
+	}
+
+}
